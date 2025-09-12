b61936847bce77e111b9891af4cc41bc489d0221
giano-montano <gianomontanoc@gmail.com>
2025-09-11 21:45:01 -0500

    hagan logs en su algoritmo

--- pdds-backend/src/main/java/pe/edu/pucp/inf/pddsbackend/algorithms/TabuSearchAlgorithmStrategy.java
+++ pdds-backend/src/main/java/pe/edu/pucp/inf/pddsbackend/algorithms/TabuSearchAlgorithmStrategy.java
@@ -1,17 +1,18 @@
 package pe.edu.pucp.inf.pddsbackend.algorithms;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import pe.edu.pucp.inf.pddsbackend.algorithms.model.*;
+import pe.edu.pucp.inf.pddsbackend.algorithms.utils.LoggingReport;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class TabuSearchAlgorithmStrategy implements PlanificationStrategy {
-
+    // pa logs
+    private LoggingReport loggingReport = new LoggingReport();
     @Override
-    public PlanificationSolutionOutput planificar(PlanificationProblemInput parametrosAlgoritmo) {
+    public PlanificationSolutionOutput planificar(PlanificationProblemInput parametrosAlgoritmo) throws Exception {
 
         // Leer input (defensivo: crear copias mutables)
         List<PedidoForAlgorithm> pedidos = parametrosAlgoritmo.pedidos() == null? new ArrayList<>(): new ArrayList<>(parametrosAlgoritmo.pedidos());
@@ -22,6 +23,12 @@ public class TabuSearchAlgorithmStrategy implements PlanificationStrategy {
         /*
         * Aquí comiencen a codear el algoritmo
         * */
+
+        // Ejemplo de hacer log en reporte externo [IMPORTANTE PARA QUE LE HAGAS SEGUIMIENTO A TU ALGORITMO]
+        loggingReport.appendReport("Inicio de planificacion con algoritmo tabú. Pedidos: " + pedidos.size() + ", vuelos=" + vuelos.size() + ", almacenes=" + almacenes.size());
+
+        // Ejemplo de guardar el reporte en /reports
+        loggingReport.writeReportFile("tabu-report-final");
 
         return null;
     }
